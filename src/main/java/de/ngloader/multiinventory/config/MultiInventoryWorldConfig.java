eb653960ad99ebb74b1451b7bb24f963008d5f95
Nils Gereke <nilsgereke1@gmail.com>
2018-07-10 09:20:23 +0200

    released version 1.0.0

--- src/main/java/de/ngloader/multiinventory/config/MultiInventoryWorldConfig.java
+++
@@ -1,9 +0,0 @@
-package de.ngloader.multiinventory.config;
-
-public class MultiInventoryWorldConfig {
-
-	public String inventoryId;
-	public String templateId;
-
-	public MultiInvetoryWorldConfigSettings settings;
-}
